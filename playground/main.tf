f3894988728c39ada5c86583751090c94aea0f21
Dawei Hu <hoodavy@gmail.com>
2025-05-12 21:57:21 +0800

    output.tf

--- playground/main.tf
+++ playground/main.tf
@@ -2,8 +2,23 @@ provider "aws" {
     region = "ap-southeast-1"
 }
 
-resource "aws_instance" "example" {
-    ami           = "ami-01938df366ac2d954"
-    instance_type = "t2.micro"
-    key_name = "aws-login"
+variable "ami_id" {
+  description = "EC2 AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 intance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key name for login"
+  type        = string
+}
+
+resource "aws_instance" "demo" {
+    ami           = var.ami_id
+    instance_type = var.instance_type
+    key_name      = var.key_name
 }
---
+++ playground/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_id = "ami-01938df366ac2d954"
+instance_type = "t2.micro"
+key_name      = "aws-login"

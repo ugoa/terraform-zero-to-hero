5a04259fbdfe44d613d1bdbdaf5c2baf68f74d21
Dawei Hu <hoodavy@gmail.com>
2025-05-13 22:23:26 +0800

    add ec2

---
+++ playground/module/ec2_instance/variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    description = "value for the ami"
+}
+
+variable "instance_type" {
+    description = "value for instance_type"
+}
+
+variable "key_name" {
+    description = "value for the subnet_id"
+}
